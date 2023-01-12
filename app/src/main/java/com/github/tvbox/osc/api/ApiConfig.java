99e9176c36f11e6013c61d838ba5c226d3663bf4
912127746 <41746382+912127746@users.noreply.github.com>
2023-01-12 21:54:42 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://jx.jifk.nl/d/gtbKvnE1.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
